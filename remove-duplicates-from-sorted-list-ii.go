fe3747c3c85bf109338796737773671f0143d6bf
Daniel Lin <qienlin@outlook.com>
2016-05-15 22:48:05 +0800

    Add remove-duplicates-from-sorted-list-ii

---
+++ remove-duplicates-from-sorted-list-ii.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func (head *ListNode) String() string {
+	str := ""
+	for cur := head; cur != nil; cur = cur.Next {
+		str += fmt.Sprintf("->%d", cur.Val)
+	}
+	return str
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	m := make(map[int]int)
+	for cur := head; cur != nil; cur = cur.Next {
+		m[cur.Val]++
+	}
+	for ; head != nil && m[head.Val] > 1; head = head.Next {
+	}
+	if head == nil || head.Next == nil {
+		return head
+	}
+	for pre, cur := head, head.Next; cur != nil; {
+		if m[cur.Val] > 1 {
+			pre.Next, cur = cur.Next, cur.Next
+		} else {
+			pre, cur = pre.Next, cur.Next
+		}
+	}
+	return head
+}
+
+func main() {
+	head := &ListNode{1, &ListNode{1, nil}}
+	fmt.Println(deleteDuplicates(head))
+}
